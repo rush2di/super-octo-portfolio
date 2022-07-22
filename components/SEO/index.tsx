3865a00baf0a7a4ff4508ffe6149a0179d739b75
rush2di <roch2di@gmail.com>
2022-07-22 13:44:19 +0100

    add SEO & metadata

---
+++ components/SEO/index.tsx
@@ -0,0 +1,49 @@
+import Head from "next/head";
+import { SeoProps } from "./types";
+
+const SEO = ({ title, description }: SeoProps) => {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta property="og:image" content="/images/thumbnail_main.png" />
+        <meta property="og:site_name" content="ROCHDI BELHIRCH" />
+        <meta property="og:title" content={`ROCHDI BELHIRCH | ${title}`} />
+        <meta property="og:description" content={description} />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:title" content={`ROCHDI BELHIRCH | ${title}`} />
+        <meta name="twitter:image" content="/images/thumbnail_main.png" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta
+          name="keywords"
+          content="web developer,frontend specialist,React developer,Rochdi Belhirch,mobile developer,UI/UX design,web designer,React developer"
+        ></meta>
+        <title>ROCHDI BELHIRCH | {title}</title>
+      </Head>
+    </>
+  );
+};
+
+export default SEO;
--- pages/index.tsx
+++ pages/index.tsx
@@ -18,112 +18,124 @@ import {
 } from "../constants";
 import image1 from "../public/images/hero.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SEO from "components/SEO";
 
 const Home: NextPage = () => {
   return (
-    <Layout>
-      <div className="container">
-        <section className="relative bg-gray-5">
-          <div className="bg-gray-5 w-18 sm:w-[50%] mx-auto md:w-full flex items-center justify-center mt-5">
-            <Image src={image1} placeholder="blur" className="mx-4" />
-          </div>
-          <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col h-[500px] xl:h-[486px]">
-            <h1
-              id="main_heading"
-              className="font-comp text-3xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-7xl my-auto leading-[1.4] sm:leading-[1.2] text-center mx-auto sm:mx-0 sm:text-left md:mb-3-25"
-            >
-              <span className="block capitalize overflow-hidden mix-blend-difference text-gray-200">
-                <motion.div
-                  initial="hidden"
-                  animate="visible"
-                  variants={animationVariants[0]}
-                >
-                  {heroContent.title[0]}
-                </motion.div>
-              </span>
-              <span className="block capitalize overflow-hidden mix-blend-difference text-gray-200">
-                <motion.div
-                  initial="hidden"
-                  animate="visible"
-                  variants={animationVariants[1]}
-                >
-                  {heroContent.title[1]}
-                </motion.div>
-              </span>
-              <span className="block capitalize overflow-hidden mix-blend-difference text-gray-200">
-                <motion.div
-                  initial="hidden"
-                  animate="visible"
-                  variants={animationVariants[2]}
-                >
-                  {heroContent.title[2]}
-                </motion.div>
-              </span>
-            </h1>
-            <div className="flex w-full sm:grid sm:grid-cols-6 lg:grid-cols-12 items-center">
-              <div className="hidden sm:block min-w-full h-[1px] bg-gray-5 mix-blend-difference sm:col-span-3 md:col-span-4 lg:col-span-9"></div>
-              <div className="px-1 w-full sm:col-span-3 md:col-span-2 lg:col-span-3">
-                <p className="text-sm text-center sm:text-left">
-                  {heroContent.comp}
-                </p>
+    <>
+      <SEO
+        title={`Portfolio`}
+        description={`A creative developer building modern websites with the latest technologies. I specialize frontend development bringing great designs to life with tools like React and JavaScript animation libraries.`}
+      />
+      <Layout>
+        <div className="container">
+          <section className="relative bg-gray-5">
+            <div className="bg-gray-5 w-18 sm:w-[50%] mx-auto md:w-full flex items-center justify-center mt-5">
+              <Image src={image1} placeholder="blur" className="mx-4" />
+            </div>
+            <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col h-[500px] xl:h-[486px]">
+              <h1
+                id="main_heading"
+                className="font-comp text-3xl sm:text-5xl md:text-6xl lg:text-6xl xl:text-7xl my-auto leading-[1.4] sm:leading-[1.2] text-center mx-auto sm:mx-0 sm:text-left md:mb-3-25"
+              >
+                <span className="block capitalize overflow-hidden mix-blend-difference text-gray-200">
+                  <motion.div
+                    initial="hidden"
+                    animate="visible"
+                    variants={animationVariants[0]}
+                  >
+                    {heroContent.title[0]}
+                  </motion.div>
+                </span>
+                <span className="block capitalize overflow-hidden mix-blend-difference text-gray-200">
+                  <motion.div
+                    initial="hidden"
+                    animate="visible"
+                    variants={animationVariants[1]}
+                  >
+                    {heroContent.title[1]}
+                  </motion.div>
+                </span>
+                <span className="block capitalize overflow-hidden mix-blend-difference text-gray-200">
+                  <motion.div
+                    initial="hidden"
+                    animate="visible"
+                    variants={animationVariants[2]}
+                  >
+                    {heroContent.title[2]}
+                  </motion.div>
+                </span>
+              </h1>
+              <div className="flex w-full sm:grid sm:grid-cols-6 lg:grid-cols-12 items-center">
+                <div className="hidden sm:block min-w-full h-[1px] bg-gray-5 mix-blend-difference sm:col-span-3 md:col-span-4 lg:col-span-9"></div>
+                <div className="px-1 w-full sm:col-span-3 md:col-span-2 lg:col-span-3">
+                  <p className="text-sm text-center sm:text-left">
+                    {heroContent.comp}
+                  </p>
+                </div>
               </div>
             </div>
+          </section>
+          <div className="mt-7 sm:mt-12 md:mt-3 lg:mt-3">
+            <Scroll />
+          </div>
+          <section id="about" className="px-1 sm:px-0-5">
+            <div className="row mt-3">
+              <div className="col-12 md:col-6">
+                <CopyWriting
+                  title={introContent.about.title}
+                  text={introContent.about.text}
+                />
+              </div>
+              <div className="col-12 md:col-6">
+                <CopyWriting
+                  title={introContent.technicalSkills.title}
+                  text={introContent.technicalSkills.text}
+                />
+              </div>
+            </div>
+          </section>
+        </div>
+        <section
+          id="projects"
+          className="mt-5 relative z-20 w-full mx-auto bg-gray-5 shadow-gray-5 shadow-[0_0_120px_110px] py-2"
+        >
+          {projects.map(({ id, initializer, ...data }) => (
+            <div key={uuid()} className="project">
+              <ProjectWrapper id={id} initializer={initializer}>
+                <ProjectContent {...data} />
+              </ProjectWrapper>
+            </div>
+          ))}
+        </section>
+        <section id="connect">
+          <div className="container my-2 py-5 flex items-center justify-center relative z-20">
+            <div className="w-20">
+              <h1 className="txt-h3 italic text-gray-900 text-center mb-2">
+                {outroContent.text}
+              </h1>
+              <ul className="flex items-center justify-between px-1-5">
+                <li className="hover:opacity-60">
+                  <a href={outroContent.github} target="_blank">
+                    <FontAwesomeIcon icon={["fab", "github"]} size="2x" />
+                  </a>
+                </li>
+                <li className="hover:opacity-60">
+                  <a href={outroContent.linkedin} target="_blank">
+                    <FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" />
+                  </a>
+                </li>
+                <li className="hover:opacity-60">
+                  <a href={"mailto:" + outroContent.email}>
+                    <FontAwesomeIcon icon={["fas", "envelope"]} size="2x" />
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </section>
-        <div className="mt-7 sm:mt-12 md:mt-3 lg:mt-3">
-          <Scroll />
-        </div>
-        <section id="about" className="px-1 sm:px-0-5">
-          <div className="row mt-3">
-            <div className="col-12 md:col-6">
-              <CopyWriting
-                title={introContent.about.title}
-                text={introContent.about.text}
-              />
-            </div>
-            <div className="col-12 md:col-6">
-              <CopyWriting
-                title={introContent.technicalSkills.title}
-                text={introContent.technicalSkills.text}
-              />
-            </div>
-          </div>
-        </section>
-      </div>
-      <section id="projects" className="mt-5 relative z-20 w-full mx-auto bg-gray-5 shadow-gray-5 shadow-[0_0_120px_110px] py-2">
-        {projects.map(({ id, initializer, ...data }) => (
-          <div key={uuid()} className="project">
-            <ProjectWrapper id={id} initializer={initializer}>
-              <ProjectContent {...data} />
-            </ProjectWrapper>
-          </div>
-        ))}
-      </section>
-      <section id="connect">
-        <div className="container my-2 py-5 flex items-center justify-center relative z-20">
-          <div className="w-20">
-            <h1 className="txt-h3 italic text-gray-900 text-center mb-2">{outroContent.text}</h1>
-            <ul className="flex items-center justify-between px-1-5">
-              <li className="hover:opacity-60">
-                <a href={outroContent.github} target="_blank">
-                  <FontAwesomeIcon icon={["fab", "github"]} size="2x" />
-                </a>
-              </li>
-              <li className="hover:opacity-60">
-                <a href={outroContent.linkedin} target="_blank">
-                  <FontAwesomeIcon icon={["fab", "linkedin"]} size="2x" />
-                </a>
-              </li>
-              <li className="hover:opacity-60">
-                <a href={"mailto:" + outroContent.email}>
-                  <FontAwesomeIcon icon={["fas", "envelope"]} size="2x" />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </section>
-    </Layout>
+      </Layout>
+    </>
   );
 };
 
